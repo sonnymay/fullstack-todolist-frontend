e2db16bdc6e4da63e31d017a0f95bc17fa4fe016
Sonny May <102835434+sonnymay@users.noreply.github.com>
2023-08-29 11:32:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
 import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
 
-
 function App() {
 
-  const [toDo, setToDo] = useState([])
-  const [text, setText] = useState("")
-  const [isUpdating, setIsUpdating] = useState(false)
-  const [toDoId, setToDoId] = useState("")
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("");
+  const [showFirework, setShowFirework] = useState(false);
 
   useEffect(() => {
-    getAllToDo(setToDo)
-  }, [])
+    getAllToDo(setToDo);
+  }, []);
 
   const updateMode = (_id, text) => {
-    setIsUpdating(true)
-    setText(text)
-    setToDoId(_id)
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id);
+  }
+
+  const launchFirework = () => {
+    setShowFirework(true);
+    setTimeout(() => setShowFirework(false), 700); // Match the duration of the firework animation
   }
 
   return (
     <div className="App">
+
+      {showFirework && <div className="firework"></div>}
 
       <div className="container">
 
@@ -38,8 +45,13 @@ function App() {
           <div
             className="add"
             onClick={isUpdating ?
-              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
-              : () => addToDo(text, setText, setToDo)}>
+              () => {
+                updateToDo(toDoId, text, setToDo, setText, setIsUpdating);
+              }
+              : () => {
+                addToDo(text, setText, setToDo);
+              }
+            }>
             {isUpdating ? "Update" : "Add"}
           </div>
 
@@ -47,11 +59,17 @@ function App() {
 
         <div className="list">
 
-          {toDo.map((item) => <ToDo 
-          key={item._id} 
-          text={item.text}
-          updateMode = {() => updateMode(item._id, item.text)}
-          deleteToDo = {() => deleteToDo(item._id, setToDo)} />)}
+          {toDo.map((item) => (
+            <ToDo 
+              key={item._id} 
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => {
+                deleteToDo(item._id, setToDo);
+                launchFirework();
+              }}
+            />
+          ))}
 
         </div>
 
