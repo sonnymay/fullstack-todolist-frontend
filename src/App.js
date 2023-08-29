3a1e01662257ef4008fbb60f90a2c694a8a3f0f0
Sonny May <102835434+sonnymay@users.noreply.github.com>
2023-08-29 11:43:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,44 @@ import ToDo from "./components/ToDo";
 import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
 
 function App() {
-
   const [toDo, setToDo] = useState([]);
   const [text, setText] = useState("");
   const [isUpdating, setIsUpdating] = useState(false);
   const [toDoId, setToDoId] = useState("");
-  const [showFirework, setShowFirework] = useState(false);
 
   useEffect(() => {
     getAllToDo(setToDo);
   }, []);
 
+  const launchFirework = () => {
+    const firework = document.createElement('div');
+    firework.className = 'firework';
+    firework.style.top = `${Math.random() * window.innerHeight}px`;
+    firework.style.left = `${Math.random() * window.innerWidth}px`;
+    firework.style.background = `radial-gradient(circle, rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1), rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1), rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1))`;
+    document.body.appendChild(firework);
+    
+    setTimeout(() => {
+        firework.remove();
+    }, 1000); // Removes firework after animation completes
+  }
+
+  const launchMultipleFireworks = () => {
+    for (let i = 0; i < 5; i++) { 
+        setTimeout(launchFirework, i * 100);
+    }
+  };
+
   const updateMode = (_id, text) => {
     setIsUpdating(true);
     setText(text);
     setToDoId(_id);
-  }
-
-  const launchFirework = () => {
-    setShowFirework(true);
-    setTimeout(() => setShowFirework(false), 700); // Match the duration of the firework animation
-  }
+  };
 
   return (
     <div className="App">
-
-      {showFirework && <div className="firework"></div>}
-
       <div className="container">
-
         <h1>To-Do List App</h1>
-
         <div className="top">
           <input
             type="text"
@@ -41,40 +48,33 @@ function App() {
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
-
           <div
             className="add"
-            onClick={isUpdating ?
+            onClick={isUpdating ? 
               () => {
                 updateToDo(toDoId, text, setToDo, setText, setIsUpdating);
-              }
-              : () => {
+              } 
+              : 
+              () => {
                 addToDo(text, setText, setToDo);
-              }
-            }>
+              }}>
             {isUpdating ? "Update" : "Add"}
           </div>
-
         </div>
-
         <div className="list">
-
-          {toDo.map((item) => (
+          {toDo.map((item) => 
             <ToDo 
               key={item._id} 
               text={item.text}
               updateMode={() => updateMode(item._id, item.text)}
               deleteToDo={() => {
                 deleteToDo(item._id, setToDo);
-                launchFirework();
+                launchMultipleFireworks();
               }}
             />
-          ))}
-
+          )}
         </div>
-
       </div>
-
     </div>
   );
 }
